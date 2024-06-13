581f3fee1be5dd8b0ca7d3b0005234926e5bd3f6
Aiden Oliver <31047390+AidenOliver@users.noreply.github.com>
2024-06-12 19:43:18 -0600

    First commit

---
+++ app/src/main/java/com/example/mapchat/AndroidBluetoothController.kt
@@ -0,0 +1,42 @@
+package com.example.mapchat
+
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import com.example.mapchat.domain.chat.BluetoothController
+import com.example.mapchat.domain.chat.BluetoothDevice
+import com.example.mapchat.domain.chat.BluetoothDeviceDomain
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class AndroidBluetoothController(
+    private val context: Context
+): BluetoothController {
+
+    private val bluetoothManager by lazy {
+        context.getSystemService(BluetoothManager::class.java)
+    }
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val _scannedDevices = MutableStateFlow<List<BluetoothDeviceDomain>>(emptyList())
+    override val scannedDevices: StateFlow<List<BluetoothDeviceDomain>>
+        get() = _scannedDevices.asStateFlow()
+
+    private val _pairedDevices = MutableStateFlow<List<BluetoothDeviceDomain>>(emptyList())
+    override val pairedDevices: StateFlow<List<BluetoothDeviceDomain>>
+        get() = _pairedDevices.asStateFlow()
+
+    override fun startDiscovery() {
+        TODO("Not yet implemented")
+    }
+
+    override fun stopDiscovery() {
+        TODO("Not yet implemented")
+    }
+
+    override fun release() {
+        TODO("Not yet implemented")
+    }
+}
